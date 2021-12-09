24d0f86b3a7143d3ac4c672743c1f21d9439bc34
ipins46 <muhamadarifin428@gmail.com>
2021-12-10 01:36:13 +0700

    authentication up

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function HomePage({events}) {
         <h1>Upcoming Events</h1>
         {events.length === 0 && <h3>No events to show</h3>}
 
-        {events.map(evt => (
+        {events.map((evt) => (
           <EventItem key={evt.id} evt={evt}/>
         ))}
 
@@ -31,11 +31,11 @@ export default function HomePage({events}) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`)
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`)
   const events = await res.json()
 
   return {
-    props: {events: events.slice(0, 3)},
+    props: {events},
     revalidate: 1
   }
 }
